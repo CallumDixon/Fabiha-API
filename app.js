caf93c31b547722ad9b8c742641404997b53a65b
Callum Dixon <callum.dixon002@gmail.com>
2021-12-05 12:33:30 +0000

    init

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+var MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://callum:Dicko941@192.168.0.69:27017/test"
+var itemsDB
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    itemsDB = db.db("test").collection("items")
+    /* var myobj = { name: 'i'};
+    dbo.collection("customers").insertOne(myobj, function(err, res) {
+    if (err) throw err
+    }) */
+
+
+    //itemsDB.insertMany([{name: 'Crisps',quantity:'4'},{name: 'Bread',quantity:'1'}])
+  })
+
+app.get('/', (req, res) => {
+    itemsDB.find({name:'Crisps'}).toArray((err,result)=>{
+        res.send(result)
+    })
+})
+
+app.get('/items',function(req,res){
+    itemsDB.find({name:req.query.item}).toArray((err,result)=>{
+        res.send(result)
+    })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
