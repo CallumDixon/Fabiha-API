c562a8abb9b11f32296c6b24b3766a650878a12d
Callum Dixon <callum.dixon002@gmail.com>
2021-12-19 22:36:01 +0000

    added html explaination at root and a post request

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3500
-
+app.use(express.json())
 var MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://callum:Dicko941@192.168.0.68:27017/fabiha"
 var itemsDB
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     itemsDB = db.db("fabiha").collection("pantry")
-    /* var myobj = { name: 'i'};
-    dbo.collection("customers").insertOne(myobj, function(err, res) {
-    if (err) throw err
-    }) */
-
-
-    //itemsDB.insertMany([{name: 'Crisps',quantity:'4'},{name: 'Bread',quantity:'1'}])
   })
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/root.html'))
+})
 
 app.get('/all', (req, res) => {
     itemsDB.find({}).toArray((err,result)=>{
@@ -23,11 +21,28 @@ app.get('/all', (req, res) => {
     })
 })
 
-app.get('/items',function(req,res){
+app.route('/items')
+  .get((req, res) => {
     itemsDB.find({name:req.query.item}).toArray((err,result)=>{
         res.send(result)
     })
-})
+  })
+  .post((req, res) => {
+    if(req.body.hasOwnProperty('name') && req.body.hasOwnProperty('quantity')){
+
+        if(req.body.quantity > 0){
+            res.send("success")
+            console.log(req.body.name)
+            var myobj = { name: req.body.name, quantity: req.body.quantity}
+            itemsDB.insertOne(myobj, (err, res) => {if (err) throw err})
+        } 
+    }
+    else{
+        res.send("failure")
+    }
+
+    //itemsDB.insertMany([{name: 'Crisps',quantity:'4'},{name: 'Bread',quantity:'1'}])
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}/all`)
